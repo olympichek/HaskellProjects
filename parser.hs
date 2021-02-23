c5a350d6cdad3c5b843a33a23809d6d9482e8c4b
olympichek <olympichek1@gmail.com>
2021-02-23 08:01:25 +0200

    brackets bug fixed

--- parser.hs
+++ parser.hs
@@ -1,3 +1,4 @@
+import System.IO       ( hFlush, stdout )
 import Text.Read       ( readMaybe )
 import Data.List.Split ( splitOn )
 import Control.Monad   ( (<=<), (>=>) )
@@ -22,13 +23,13 @@ changeBrackets '[' = '('
 changeBrackets ']' = ')'
 changeBrackets c   = c
 
-splitBy :: Eq a => a -> [a] -> [[a]]
-splitBy c = splitOn [c] >>> concatMap (: [[c]]) >>> init
+splitBy :: String -> String -> [String]
+splitBy s = splitOn s >>> concatMap (: [s]) >>> init
 
 splitAll :: String -> [String]
-splitAll = foldr1 (<=<) $ map splitBy spChars
+splitAll = foldr1 (<=<) $ splitBy . pure <$> spChars
 
-data Error = SyntaxError | ParseError | BracketsMismatch 
+data Error = LexicalError | ParseError | BracketsMismatch 
     deriving (Show, Eq)
 
 asToken :: String -> Either Error Token
@@ -36,7 +37,7 @@ asToken s = case asSpToken of
     Just t  -> Right t
     Nothing -> case asNumber of
         Just n  -> Right . Number $ n
-        Nothing -> Left SyntaxError
+        Nothing -> Left LexicalError
     where
     spStrings = map pure spChars
     asSpToken = lookup s $ zip spStrings spTokens
@@ -83,23 +84,23 @@ action Minus = (:-:)
 action Mul   = (:*:)
 action Div   = (:/:)
 
-build :: [Expr] -> [Expr -> Expr -> Expr] -> Either Error Expr
-build [v]        []       = Right v
-build (v1:v2:vs) (op:ops) = build (v1 `op` v2 : vs) ops
-build _          _        = Left ParseError
+buildExpr :: [Expr] -> [Expr -> Expr -> Expr] -> Either Error Expr
+buildExpr [v]        []       = Right v
+buildExpr (v1:v2:vs) (op:ops) = buildExpr (v1 `op` v2 : vs) ops
+buildExpr _          _        = Left ParseError
 
-solveMulDiv :: [Token] -> Either Error Expr
-solveMulDiv ts = build (Val <$> ns) (action <$> ops)
+buildMulDiv :: [Token] -> Either Error Expr
+buildMulDiv ts = buildExpr (Val <$> ns) (action <$> ops)
     where
     (ns, ops) = foldr split ([], []) ts
     split t (ns, ops) = case t of
         Number n -> (n:ns, ops)
         _        -> (ns, t:ops)
     
-solvePlusMinus :: [Token] -> Either Error Expr
-solvePlusMinus ts = do
-    exprs <- mapM solveMulDiv tss
-    build exprs (action <$> ops)
+buildPlusMinus :: [Token] -> Either Error Expr
+buildPlusMinus ts = do
+    exprs <- mapM buildMulDiv tss
+    buildExpr exprs (action <$> ops)
     where
     tss = concatMap (splitOn [Minus]) $ splitOn [Plus] ts
     ops = filter (\x -> x == Plus || x == Minus) ts
@@ -111,8 +112,8 @@ eval (e1 :-: e2) = eval e1 - eval e2
 eval (e1 :*: e2) = eval e1 * eval e2
 eval (e1 :/: e2) = eval e1 / eval e2
 
-splitByBrackets :: [Token] -> ([Token], [Token], [Token])
-splitByBrackets ts = (lt, ct, rt) where
+splitByBrackets' :: [Token] -> ([Token], [Token], [Token])
+splitByBrackets' ts = (lt, ct, rt) where
     t1 = splitOn [LeftBrace] ts
     t2 = concat $ tail t1
     t3 = splitOn [RightBrace] t2
@@ -120,21 +121,37 @@ splitByBrackets ts = (lt, ct, rt) where
     ct = concat $ init t3
     rt = last t3
 
+splitByBrackets :: [Token] -> [[Token]]
+splitByBrackets [] = [[]]
+splitByBrackets ts = helper ts 0 [[]]  where
+    helper []     _ res = res
+    helper (t:ts) n res = case t of
+        LeftBrace  -> if n == 0
+            then helper ts (n+1) $ res ++ [[]]
+            else helper ts (n+1) $ res +++ [t]
+        RightBrace -> if n == 1
+            then helper ts (n-1) $ res ++ [[]]
+            else helper ts (n-1) $ res +++ [t]
+        _          -> helper ts n $ res +++ [t]
+    xs +++ ys = init xs ++ [last xs ++ ys]
+
 compute :: [Token] -> Either Error Double
-compute ts
-    | checkBrackets ts == Just True  = nt >>= compute
-    | checkBrackets ts == Just False = eval <$> solvePlusMinus ts
-    | otherwise                      = Left BracketsMismatch
+compute ts = case checkBrackets ts of
+    Just True  -> nts >>= compute
+    Just False -> eval <$> buildPlusMinus ts
+    Nothing    -> Left BracketsMismatch
     where
-    (lt, ct, rt) = splitByBrackets ts
-    nt = do
-        res <- compute ct
-        return $ lt ++ [Number res] ++ rt
+    nts = helper $ splitByBrackets ts
+    helper [p1] = Right p1
+    helper (p1:p2:ps) = do
+        r <- compute p2
+        rs <- helper ps
+        return $ p1 ++ [Number r] ++ rs
 
 main :: IO ()
 main = do
-    putStr "Enter expression: "
+    putStr "Enter expression: " >> hFlush stdout
     expr <- getLine
-    case (tokenize >=> compute) expr of
+    case tokenize >=> compute $ expr of
         Right n -> putStrLn $ "Result: " ++ show n
         Left e  -> putStrLn $ "Error: "  ++ show e
